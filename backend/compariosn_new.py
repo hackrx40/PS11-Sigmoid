6a7452f0471c05a2c6f615dcf734afe6c2c50f2b
gsabarinath02 <gsabarinath02@gmaill.com>
2023-07-21 13:15:26 +0530

    image compariosn tested - results in folder

--- backend/compariosn_new.py
+++ backend/compariosn_new.py
@@ -71,7 +71,7 @@ for filename in os.listdir(folder1_path):
         _, thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for contour in contours:
-            if cv2.contourArea(contour) > 100:
+            if cv2.contourArea(contour) > 150:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(comparison_image, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
